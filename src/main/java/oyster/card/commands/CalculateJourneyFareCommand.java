1c3e324c7657abb34c8791b0697ac9ce80a8b13f
Gu1f3f3 <gustavo_di_domenico@gap.com>
2018-05-19 16:15:34 -0700

    Add zone one crossed query to the fare calculator.

--- src/main/java/oyster/card/commands/CalculateJourneyFareCommand.java
+++ src/main/java/oyster/card/commands/CalculateJourneyFareCommand.java
@@ -4,6 +4,7 @@ import oyster.card.models.Fare;
 import oyster.card.models.TransportType;
 import oyster.card.models.Journey;
 import oyster.card.queries.GetMinimumZonesCrossedQuery;
+import oyster.card.queries.IsZoneOneCrossedQuery;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -14,17 +15,17 @@ import static java.util.Comparator.comparing;
 class CalculateJourneyFareCommand {
     private final List<Fare> fares;
     private final GetMinimumZonesCrossedQuery getMinimumZonesCrossedQuery;
+    private final IsZoneOneCrossedQuery isZoneOneCrossedQuery;
 
-    CalculateJourneyFareCommand(List<Fare> fares, GetMinimumZonesCrossedQuery getMinimumZonesCrossedQuery) {
+    CalculateJourneyFareCommand(List<Fare> fares, GetMinimumZonesCrossedQuery getMinimumZonesCrossedQuery, IsZoneOneCrossedQuery isZoneOneCrossedQuery) {
         this.fares = fares;
         this.getMinimumZonesCrossedQuery = getMinimumZonesCrossedQuery;
+        this.isZoneOneCrossedQuery = isZoneOneCrossedQuery;
     }
 
     BigDecimal run(Journey journey) {
         int minimumCrossedZones = getMinimumZonesCrossedQuery.run(journey);
-
-        boolean isZoneOneCrossed = (journey.getOrigin().getZones().size() == 1 && journey.getOrigin().getZones().contains(1)) ||
-                (journey.getDestination().getZones().size() == 1 && journey.getDestination().getZones().contains(1));
+        boolean isZoneOneCrossed = isZoneOneCrossedQuery.run(journey);
 
         Optional<Fare> minimumFare = fares.stream().filter(f ->
                 (journey.getTransportType() == TransportType.BUS && f.getTransportType() == journey.getTransportType())
--- src/test/java/oyster/card/commands/CalculateJourneyFareCommandTest.java
+++ src/test/java/oyster/card/commands/CalculateJourneyFareCommandTest.java
@@ -6,8 +6,9 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import oyster.card.models.Journey;
-import oyster.card.queries.GetMinimumZonesCrossedQuery;
 import oyster.card.models.TransportType;
+import oyster.card.queries.GetMinimumZonesCrossedQuery;
+import oyster.card.queries.IsZoneOneCrossedQuery;
 import oyster.card.support.FareFactory;
 
 import java.math.BigDecimal;
@@ -15,68 +16,73 @@ import java.math.BigDecimal;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.when;
+import static oyster.card.support.JourneyFactory.newJourney;
 import static oyster.card.support.StationFactory.earlsCourt;
 import static oyster.card.support.StationFactory.hammersmith;
 import static oyster.card.support.StationFactory.holborn;
 import static oyster.card.support.StationFactory.wimbledon;
-import static oyster.card.support.JourneyFactory.newJourney;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CalculateJourneyFareCommandTest {
     @Mock
     private GetMinimumZonesCrossedQuery getMinimumZonesCrossedQuery;
 
+    @Mock
+    private IsZoneOneCrossedQuery isZoneOneCrossedQuery;
+
     private CalculateJourneyFareCommand calculateJourneyFareCommand;
 
     @Before
     public void setUp() {
-        calculateJourneyFareCommand = new CalculateJourneyFareCommand(FareFactory.fares(), getMinimumZonesCrossedQuery);
+        calculateJourneyFareCommand =
+                new CalculateJourneyFareCommand(FareFactory.fares(), getMinimumZonesCrossedQuery, isZoneOneCrossedQuery);
     }
 
     @Test
     public void fromHolbornToEarlsCourtByTrain_ShouldBeInZoneOne() {
-        BigDecimal actualFare = calculateJourneyFareForCrossedZones(newJourney(holborn(), earlsCourt(), TransportType.TRAIN), 1);
+        BigDecimal actualFare = calculateJourneyFareForCrossedZones(newJourney(holborn(), earlsCourt(), TransportType.TRAIN), 1, true);
 
         assertThat(actualFare, is(FareFactory.anywhereInZoneOne().getValue()));
     }
 
     @Test
     public void fromEarlsCourtToHammersmithByTrain_ShouldBeOneZoneExcludingZoneOne() {
-        BigDecimal actualFare = calculateJourneyFareForCrossedZones(newJourney(earlsCourt(), hammersmith(), TransportType.TRAIN), 1);
+        BigDecimal actualFare = calculateJourneyFareForCrossedZones(newJourney(earlsCourt(), hammersmith(), TransportType.TRAIN), 1, false);
 
         assertThat(actualFare, is(FareFactory.anyOneZoneOutsideZoneOne().getValue()));
     }
 
     @Test
     public void fromHolbornToHammersmithByTrain_ShouldBeTwoZones() {
-        BigDecimal actualFare = calculateJourneyFareForCrossedZones(newJourney(holborn(), hammersmith(), TransportType.TRAIN), 2);
+        BigDecimal actualFare = calculateJourneyFareForCrossedZones(newJourney(holborn(), hammersmith(), TransportType.TRAIN), 2, true);
 
         assertThat(actualFare, is(FareFactory.anyTwoZonesIncludingZoneOne().getValue()));
     }
 
     @Test
     public void fromWimbledonToHammersmithByTrain_ShouldBeTwoZonesExcludingZoneOne() {
-        BigDecimal actualFare = calculateJourneyFareForCrossedZones(newJourney(wimbledon(), hammersmith(), TransportType.TRAIN), 2);
+        BigDecimal actualFare = calculateJourneyFareForCrossedZones(newJourney(wimbledon(), hammersmith(), TransportType.TRAIN), 2, false);
 
         assertThat(actualFare, is(FareFactory.anyTwoZonesExcludingZoneOne().getValue()));
     }
 
     @Test
     public void fromHolbornToWimbledomByTrain_ShouldBeThreeZones() {
-        BigDecimal actualFare = calculateJourneyFareForCrossedZones(newJourney(holborn(), wimbledon(), TransportType.TRAIN), 3);
+        BigDecimal actualFare = calculateJourneyFareForCrossedZones(newJourney(holborn(), wimbledon(), TransportType.TRAIN), 3, true);
 
         assertThat(actualFare, is(FareFactory.anyThreeZones().getValue()));
     }
 
     @Test
     public void fromHolbornToWimbledonByBus_ShouldBeBusJourney() {
-        BigDecimal actualFare = calculateJourneyFareForCrossedZones(newJourney(holborn(), wimbledon(), TransportType.BUS), 3);
+        BigDecimal actualFare = calculateJourneyFareForCrossedZones(newJourney(holborn(), wimbledon(), TransportType.BUS), 3, true);
 
         assertThat(actualFare, is(FareFactory.anyBusJourney().getValue()));
     }
 
-    private BigDecimal calculateJourneyFareForCrossedZones(Journey journey, int minimumCrossedZones) {
+    private BigDecimal calculateJourneyFareForCrossedZones(Journey journey, int minimumCrossedZones, boolean isZoneOneCrossed) {
         when(getMinimumZonesCrossedQuery.run(journey)).thenReturn(minimumCrossedZones);
+        when(isZoneOneCrossedQuery.run(journey)).thenReturn(isZoneOneCrossed);
 
         return calculateJourneyFareCommand.run(journey);
     }
