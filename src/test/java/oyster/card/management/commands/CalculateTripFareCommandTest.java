174ed66b9c8bf6e553c78bf52660d67d5b33855d
Gu1f3f3 <gustavo_di_domenico@gap.com>
2018-05-19 15:19:19 -0700

    Refactor calculate trip tests.

--- src/test/java/oyster/card/management/commands/CalculateTripFareCommandTest.java
+++ src/test/java/oyster/card/management/commands/CalculateTripFareCommandTest.java
@@ -2,6 +2,8 @@ package oyster.card.management.commands;
 
 import org.junit.Before;
 import org.junit.Test;
+import oyster.card.management.models.Station;
+import oyster.card.management.models.TransportType;
 import oyster.card.management.models.Trip;
 
 import java.math.BigDecimal;
@@ -33,68 +35,51 @@ public class CalculateTripFareCommandTest {
 
     @Test
     public void tripBetweenHolbornAndEarlsCourtByTrainShouldBe_2_50() {
-        BigDecimal fare = calculateTripFareCommand.run(Trip.builder()
-                .origin(holborn())
-                .destination(earlsCourt())
-                .transportType(TRAIN)
-                .build());
+        BigDecimal fare = calculateTripFareCommand.run(buildTripFor(holborn(), earlsCourt(), TRAIN));
 
         assertThat(fare, is(anywhereInZoneOne().getValue()));
     }
 
     @Test
     public void tripBetweenEarlsCourtAndHammersmithByTrainShouldBe_2_00() {
-        BigDecimal fare = calculateTripFareCommand.run(Trip.builder()
-                .origin(earlsCourt())
-                .destination(hammersmith())
-                .transportType(TRAIN)
-                .build());
+        BigDecimal fare = calculateTripFareCommand.run(buildTripFor(earlsCourt(), hammersmith(), TRAIN));
 
         assertThat(fare, is(anyOneZoneOutsideZoneOne().getValue()));
     }
 
     @Test
     public void tripBetweenHolbornAndHammersmithByTrainShouldBe_3_00() {
-        BigDecimal fare = calculateTripFareCommand.run(Trip.builder()
-                .origin(holborn())
-                .destination(hammersmith())
-                .transportType(TRAIN)
-                .build());
+        BigDecimal fare = calculateTripFareCommand.run(buildTripFor(holborn(), hammersmith(), TRAIN));
 
         assertThat(fare, is(anyTwoZonesIncludingZoneOne().getValue()));
     }
 
     @Test
     public void tripBetweenWimbledonAndHammersmithByTrainShouldBe_2_25() {
-        BigDecimal fare = calculateTripFareCommand.run(Trip.builder()
-                .origin(wimbledon())
-                .destination(hammersmith())
-                .transportType(TRAIN)
-                .build());
+        BigDecimal fare = calculateTripFareCommand.run(buildTripFor(wimbledon(), hammersmith(), TRAIN));
 
         assertThat(fare, is(anyTwoZonesExcludingZoneOne().getValue()));
     }
 
-
     @Test
     public void tripBetweenHolbornAndWimbledomByTrainShouldBe_3_20() {
-        BigDecimal fare = calculateTripFareCommand.run(Trip.builder()
-                .origin(holborn())
-                .destination(wimbledon())
-                .transportType(TRAIN)
-                .build());
+        BigDecimal fare = calculateTripFareCommand.run(buildTripFor(holborn(), wimbledon(), TRAIN));
 
         assertThat(fare, is(anyThreeZones().getValue()));
     }
 
     @Test
     public void tripBetweenHolbornAndWimbledonByBusShouldBe_1_80() {
-        BigDecimal fare = calculateTripFareCommand.run(Trip.builder()
-                .origin(holborn())
-                .destination(wimbledon())
-                .transportType(BUS)
-                .build());
+        BigDecimal fare = calculateTripFareCommand.run(buildTripFor(holborn(), wimbledon(), BUS));
 
         assertThat(fare, is(anyBusJourney().getValue()));
+    }
+
+    private Trip buildTripFor(Station origin, Station destination, TransportType transportType) {
+        return Trip.builder()
+                .origin(origin)
+                .destination(destination)
+                .transportType(transportType)
+                .build();
     }
 }
