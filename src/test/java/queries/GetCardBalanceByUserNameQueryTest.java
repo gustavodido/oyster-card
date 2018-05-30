77a373f0c219689eaa9b5b096cb29f4ebdd6c42d
Gu1f3f3 <gustavo_di_domenico@gap.com>
2018-05-29 20:30:30 -0700

    Add static imports.

--- src/test/java/queries/GetCardBalanceByUserNameQueryTest.java
+++ src/test/java/queries/GetCardBalanceByUserNameQueryTest.java
@@ -1,21 +1,22 @@
 package queries;
 
 import exceptions.CardNotFoundException;
-import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import repositories.CardRepository;
-import stubs.CardStubs;
 
 import java.math.BigDecimal;
 
 import static java.util.Optional.empty;
 import static java.util.Optional.of;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.when;
+import static stubs.CardStubs.gustavoCard;
+import static stubs.CardStubs.invalidCard;
 
 @RunWith(MockitoJUnitRunner.class)
 public class GetCardBalanceByUserNameQueryTest {
@@ -27,17 +28,17 @@ public class GetCardBalanceByUserNameQueryTest {
 
     @Test
     public void queryByUserName_ShouldReturnCardBalance() {
-        when(cardRepository.get(CardStubs.gustavoCard().getUserName())).thenReturn(of(CardStubs.gustavoCard()));
+        when(cardRepository.get(gustavoCard().getUserName())).thenReturn(of(gustavoCard()));
 
-        BigDecimal expectedBalance = getCardBalanceByUserNameQuery.run(CardStubs.gustavoCard().getUserName());
-        assertThat(expectedBalance, CoreMatchers.is(CardStubs.gustavoCard().getBalance()));
+        BigDecimal expectedBalance = getCardBalanceByUserNameQuery.run(gustavoCard().getUserName());
+        assertThat(expectedBalance, is(gustavoCard().getBalance()));
     }
 
     @Test(expected = CardNotFoundException.class)
     public void nonExistentUserName_ShouldThrowException() {
-        when(cardRepository.get(CardStubs.invalidCard().getUserName())).thenReturn(empty());
+        when(cardRepository.get(invalidCard().getUserName())).thenReturn(empty());
 
-        getCardBalanceByUserNameQuery.run(CardStubs.invalidCard().getUserName());
+        getCardBalanceByUserNameQuery.run(invalidCard().getUserName());
     }
 
 }
