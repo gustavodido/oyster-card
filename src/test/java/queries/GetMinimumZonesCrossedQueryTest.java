822ebac6250e687c5c32fb0f453df532e4bd72cf
Gu1f3f3 <gustavo_di_domenico@gap.com>
2018-05-29 20:31:13 -0700

    Add static imports.

--- src/test/java/queries/GetMinimumZonesCrossedQueryTest.java
+++ src/test/java/queries/GetMinimumZonesCrossedQueryTest.java
@@ -4,11 +4,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.MockitoJUnitRunner;
-import stubs.JourneyStubs;
-import stubs.StationStubs;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static stubs.JourneyStubs.newJourney;
+import static stubs.StationStubs.earlsCourt;
+import static stubs.StationStubs.holborn;
+import static stubs.StationStubs.wimbledon;
 
 @RunWith(MockitoJUnitRunner.class)
 public class GetMinimumZonesCrossedQueryTest {
@@ -17,28 +19,28 @@ public class GetMinimumZonesCrossedQueryTest {
 
     @Test
     public void sameZoneJourney_ShouldBeOneZone() {
-        int zonesCrossed = getMinimumZonesCrossedQuery.run(JourneyStubs.newJourney(StationStubs.earlsCourt(), StationStubs.earlsCourt()));
+        int zonesCrossed = getMinimumZonesCrossedQuery.run(newJourney(earlsCourt(), earlsCourt()));
 
         assertThat(zonesCrossed, is(1));
     }
 
     @Test
     public void multipleOptionsJourney_ShouldBeTheMinimumPossible() {
-        int zonesCrossed = getMinimumZonesCrossedQuery.run(JourneyStubs.newJourney(StationStubs.holborn(), StationStubs.earlsCourt()));
+        int zonesCrossed = getMinimumZonesCrossedQuery.run(newJourney(holborn(), earlsCourt()));
 
         assertThat(zonesCrossed, is(1));
     }
 
     @Test
     public void fromHolbornToWimbledon_ShouldBeAllThreeZones() {
-        int zonesCrossed = getMinimumZonesCrossedQuery.run(JourneyStubs.newJourney(StationStubs.holborn(), StationStubs.wimbledon()));
+        int zonesCrossed = getMinimumZonesCrossedQuery.run(newJourney(holborn(), wimbledon()));
 
         assertThat(zonesCrossed, is(3));
     }
 
     @Test
     public void fromEarlsCourtToWimbledon_ShouldBeTwoZones() {
-        int zonesCrossed = getMinimumZonesCrossedQuery.run(JourneyStubs.newJourney(StationStubs.earlsCourt(), StationStubs.wimbledon()));
+        int zonesCrossed = getMinimumZonesCrossedQuery.run(newJourney(earlsCourt(), wimbledon()));
 
         assertThat(zonesCrossed, is(2));
     }
