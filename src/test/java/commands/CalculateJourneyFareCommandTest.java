830c7b03f2153120cfbd4f3ff3b7569ebad40166
Gu1f3f3 <gustavo_di_domenico@gap.com>
2018-05-29 20:28:47 -0700

    Add static imports.

--- src/test/java/commands/CalculateJourneyFareCommandTest.java
+++ src/test/java/commands/CalculateJourneyFareCommandTest.java
@@ -12,15 +12,26 @@ import org.mockito.junit.MockitoJUnitRunner;
 import queries.GetMinimumZonesCrossedQuery;
 import queries.IsZoneOneCrossedQuery;
 import repositories.FareRepository;
-import stubs.FareStubs;
-import stubs.JourneyStubs;
-import stubs.StationStubs;
 
 import java.math.BigDecimal;
 
+import static models.TransportType.TRAIN;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.when;
+import static stubs.FareStubs.anyBusJourney;
+import static stubs.FareStubs.anyOneZoneOutsideZoneOne;
+import static stubs.FareStubs.anyThreeZones;
+import static stubs.FareStubs.anyTwoZonesExcludingZoneOne;
+import static stubs.FareStubs.anyTwoZonesIncludingZoneOne;
+import static stubs.FareStubs.anywhereInZoneOne;
+import static stubs.FareStubs.fares;
+import static stubs.JourneyStubs.newJourney;
+import static stubs.StationStubs.chelsea;
+import static stubs.StationStubs.earlsCourt;
+import static stubs.StationStubs.hammersmith;
+import static stubs.StationStubs.holborn;
+import static stubs.StationStubs.wimbledon;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CalculateJourneyFareCommandTest {
@@ -38,61 +49,61 @@ public class CalculateJourneyFareCommandTest {
 
     @Before
     public void setUp() {
-        when(fareRepository.list()).thenReturn(FareStubs.fares());
+        when(fareRepository.list()).thenReturn(fares());
     }
 
     @Test
     public void fromHolbornToEarlsCourtByTrain_ShouldBeInZoneOneFare() {
-        BigDecimal actualFare = calculateJourneyFareForCrossedZones(JourneyStubs.newJourney(StationStubs.holborn(), StationStubs.earlsCourt(), TransportType.TRAIN), 1, true);
+        BigDecimal actualFare = calculateJourneyFareForCrossedZones(newJourney(holborn(), earlsCourt(), TRAIN), 1, true);
 
-        assertThat(actualFare, is(FareStubs.anywhereInZoneOne().getValue()));
+        assertThat(actualFare, is(anywhereInZoneOne().getValue()));
     }
 
     @Test
     public void fromEarlsCourtToHammersmithByTrain_ShouldBeOneZoneExcludingZoneOneFare() {
-        BigDecimal actualFare = calculateJourneyFareForCrossedZones(JourneyStubs.newJourney(StationStubs.earlsCourt(), StationStubs.hammersmith(), TransportType.TRAIN), 1, false);
+        BigDecimal actualFare = calculateJourneyFareForCrossedZones(newJourney(earlsCourt(), hammersmith(), TRAIN), 1, false);
 
-        assertThat(actualFare, is(FareStubs.anyOneZoneOutsideZoneOne().getValue()));
+        assertThat(actualFare, is(anyOneZoneOutsideZoneOne().getValue()));
     }
 
     @Test
     public void fromHolbornToHammersmithByTrain_ShouldBeTwoZonesFare() {
-        BigDecimal actualFare = calculateJourneyFareForCrossedZones(JourneyStubs.newJourney(StationStubs.holborn(), StationStubs.hammersmith(), TransportType.TRAIN), 2, true);
+        BigDecimal actualFare = calculateJourneyFareForCrossedZones(newJourney(holborn(), hammersmith(), TRAIN), 2, true);
 
-        assertThat(actualFare, is(FareStubs.anyTwoZonesIncludingZoneOne().getValue()));
+        assertThat(actualFare, is(anyTwoZonesIncludingZoneOne().getValue()));
     }
 
     @Test
     public void fromWimbledonToHammersmithByTrain_ShouldBeTwoZonesExcludingZoneOneFare() {
-        BigDecimal actualFare = calculateJourneyFareForCrossedZones(JourneyStubs.newJourney(StationStubs.wimbledon(), StationStubs.hammersmith(), TransportType.TRAIN), 2, false);
+        BigDecimal actualFare = calculateJourneyFareForCrossedZones(newJourney(wimbledon(), hammersmith(), TRAIN), 2, false);
 
-        assertThat(actualFare, is(FareStubs.anyTwoZonesExcludingZoneOne().getValue()));
+        assertThat(actualFare, is(anyTwoZonesExcludingZoneOne().getValue()));
     }
 
     @Test
     public void fromHolbornToWimbledomByTrain_ShouldBeThreeZonesFare() {
-        BigDecimal actualFare = calculateJourneyFareForCrossedZones(JourneyStubs.newJourney(StationStubs.holborn(), StationStubs.wimbledon(), TransportType.TRAIN), 3, true);
+        BigDecimal actualFare = calculateJourneyFareForCrossedZones(newJourney(holborn(), wimbledon(), TRAIN), 3, true);
 
-        assertThat(actualFare, is(FareStubs.anyThreeZones().getValue()));
+        assertThat(actualFare, is(anyThreeZones().getValue()));
     }
 
     @Test
     public void fromHolbornToWimbledonByBus_ShouldBeBusJourneyFare() {
-        BigDecimal actualFare = calculateJourneyFareCommand.run(JourneyStubs.newJourney(StationStubs.holborn(), StationStubs.wimbledon(), TransportType.BUS));
+        BigDecimal actualFare = calculateJourneyFareCommand.run(newJourney(holborn(), wimbledon(), TransportType.BUS));
 
-        assertThat(actualFare, is(FareStubs.anyBusJourney().getValue()));
+        assertThat(actualFare, is(anyBusJourney().getValue()));
     }
 
     @Test
     public void journeyBetweenBusStations_ShouldBeBusJourneyFare() {
-        BigDecimal actualFare = calculateJourneyFareCommand.run(JourneyStubs.newJourney(StationStubs.chelsea(), StationStubs.chelsea(), TransportType.BUS));
+        BigDecimal actualFare = calculateJourneyFareCommand.run(newJourney(chelsea(), chelsea(), TransportType.BUS));
 
-        assertThat(actualFare, is(FareStubs.anyBusJourney().getValue()));
+        assertThat(actualFare, is(anyBusJourney().getValue()));
     }
 
     @Test(expected = NoAvailableFareForJourneyException.class)
     public void journeyWithoutAvailableFare_ShouldThrowException() {
-        calculateJourneyFareForCrossedZones(JourneyStubs.newJourney(StationStubs.chelsea(), StationStubs.chelsea(), TransportType.TRAIN), 4, true);
+        calculateJourneyFareForCrossedZones(newJourney(chelsea(), chelsea(), TRAIN), 4, true);
     }
 
     private BigDecimal calculateJourneyFareForCrossedZones(Journey journey, int minimumCrossedZones, boolean isZoneOneCrossed) {
